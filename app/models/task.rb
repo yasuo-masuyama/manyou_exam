643db15b2d4e02b7a64a0babc505a258f86ab049
yasuo masuyama <y.m19980526@gmail.com>
2023-06-16 13:38:15 +0900

    model編集

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 255}
-  validates :detail, presence: true, length: { maximum: 255}
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :detail, presence: true, length: { maximum: 255 }
+
+  enum status: { not_started: 0, started: 1, completed: 2 }
+  enum priority: { high: 0, middle: 1, low: 2 }
+
+  scope :by_created_at, -> { order(created_at: :desc) }
+  scope :by_expired_at, -> { order(expired_at: :desc) }
+  scope :by_priority, -> { order(priority: :asc) }
+  scope :search_word, ->(search) { where("title LIKE ?", "%#{search}%") }
+  scope :search_status, ->(status) { where(status: status) }
 end
