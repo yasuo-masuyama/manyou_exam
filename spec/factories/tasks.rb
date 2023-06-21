773ccd17e8cd2c8947bdde893a7f3abc7f07cf50
yasuo masuyama <y.m19980526@gmail.com>
2023-06-21 12:05:51 +0900

    Rspecの編集

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :task do
+    user
     title { "1:Factoryで作ったデフォルトのタイトル" }
     detail { "1:Factoryで作ったデフォルトの詳細" }
-    expired_at { 7.days.from_now }
+    expired_at { 1.days.from_now }
     status { 0 }
     priority { 1 }
   end
   factory :second_task, class: Task do
+    user
     title { "2:Factoryで作ったデフォルトのタイトル" }
     detail { "2:Factoryで作ったデフォルトの詳細" }
     expired_at { 2.days.from_now }
@@ -14,6 +16,7 @@ FactoryBot.define do
     priority { 0 }
   end
   factory :third_task, class: Task do
+    user
     title { "3:Factoryで作ったデフォルトのタイトル" }
     detail { "3:Factoryで作ったデフォルトの詳細" }
     expired_at { 3.days.from_now }
